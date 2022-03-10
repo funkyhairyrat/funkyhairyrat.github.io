7ca99febfad8af6e8f02dc40029a13cddd8fa748
Derek Kim <funkyhairyrat@gmail.com>
2022-03-09 17:47:41 -0800

    first commit

---
+++ main.js
@@ -0,0 +1,51 @@
+/*
+Java Script 
+    What are variables
+        let someNumber = 5;
+        A varible is a refrence to some value the variable "someNumber" refers to 5
+
+    Defining variables 
+        var, const, let
+        unique identifier
+            let 5thingsToDo
+                \
+                must start with letter
+            let theNumber5 = 5
+                        \
+                        valid
+            characters you can use : letters, digits, _, $.
+
+    To variables you can assign strings, numbers, bools, functions, other variables, etc
+
+*/
+
+var color = "red"
+console.log(color)
+var anotherColor = color
+console.log(anotherColor)
+color = "blue"
+console.log(color)
+console.log(anotherColor)
+
+
+var e = {color:"red"}
+
+console.log(e)
+
+var ethesecond = {}
+ethesecond.colorthesecond = e.color
+ethesecond.colorthesecond = "blue"
+
+console.log(ethesecond)
+
+var input2 = '7'
+var math = input1 * input2
+var input1 = 'a'
+
+console.log(math)
+console.log(typeof math)
+
+console.log(a)
+
+//som othr coe
+let a = 5;
